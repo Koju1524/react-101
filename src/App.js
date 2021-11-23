50205bde690e3a49111fd92a24e116e6dbf20ada
Koju Sakaguchi <fuetf@icloud.com>
2021-11-23 16:01:59 +0900

    create counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(previousCount => previousCount + 1)
+  const multiple = () => setCount(previousCount => previousCount * 2)
+
+  const divideBy3 = () => setCount(previousCount => {
+    return previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  })
+
+  const decrement = () => setCount(previousCount => previousCount -1)
+  const reset = () => setCount(0)
+
   return (
-    <div>
-      template
-    </div>
+    <>
+      <div>
+        count: { count }
+      </div>
+      <button onClick={increment}>+1</button>
+      <button onClick={multiple}>*2</button>
+      <button onClick={divideBy3}>%3</button>
+
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>Reset</button>
+
+    </>
   )
 }
 
